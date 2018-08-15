7c76e34afff377669d577a0734b99d10ee2a4ff8
Shunichi09 <quick1st97of@gmail.com>
2018-08-16 01:49:35 +0900

    add

--- 2nd/count_base.py
+++ 2nd/count_base.py
@@ -81,6 +81,26 @@ def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
         if count >= top: # 5番目まで出力！！
             return
 
+# 相互情報量行列
+def ppmi(C, verbose=False, eps=1e-8): # 共起行列を確率ベースに進化
+    M = np.zeros_like(C, dtype=np.float32) # 空の入れ物を作っておく
+    N = np.sum(C) # 全部の合計（共起行列で母数かける）
+    S = np.sum(C, axis=0) # 各ものが列で足し算される
+    total = C.shape[0] * C.shape[1]
+    cnt = 0 # カウント
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[j] * S[i]) + eps) # 最初の項は共起した回数，別々に登場した回数
+            M[i, j] = max(0, round(pmi, 3)) # ごにゅうしとく
+
+            if verbose:
+                cnt += 1
+                if cnt % (total/100) == 0:
+                    print('{0} done'.format(100*cnt/total))
+    
+    return M
+
 def main():
     text = 'You say goodbye and I say hello.' #  I want to say hello but you say goodbye.'
     corpus, word_to_id, id_to_word = preprocess(text)
@@ -95,6 +115,13 @@ def main():
     print(cos_similarity(c0, c1))
 
     most_similar('you', word_to_id, id_to_word, C)
+
+    W = ppmi(C)
+
+    U, S, V = np.linalg.svd(W) # 特異値分解！！
+
+    print(np.round(S, 3))
+
 
 if __name__ == '__main__':
     main()
